6820a585d20e11a01e90a70380a531cbb2c67348
EtCiano <miguelfmellobirck@gmail.com>
2026-06-30 09:53:06 -0300

    tentei colocar o sistema de oclocar assets com eles sendo cenas separadas, não deu certo TODO: TODO do script do assetPanel e fazer com que, quando o mouse estiver em cima do assetPanel, o scroll não mexer no zoom, apenas no scroll do assetPanel

--- scripts/asset_panel.gd
+++ scripts/asset_panel.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var grid = $ColorRect/ScrollContainer/GridContainer
 
 const AssetButton = preload("res://scenes/asset_button.tscn")
+const AssetMap = preload("res://scenes/asset_map.tscn")
 
 func _ready() -> void:
 	_load_assets()
@@ -36,5 +37,20 @@ func _spawn_button(asset: assetData) -> void:
 	btn.pressed.connect(_on_asset_selected.bind(asset))
 
 func _on_asset_selected(asset: assetData) -> void:
-	print("Asset selecionado: ", asset.name)
+	pass
+	#var assetOnMap = AssetMap.instantiate()
+	#get_tree().current_scene.add_child(assetOnMap)
+	#assetOnMap.position = Vector2(0, 0)
+	#var texturaAsset = assetOnMap.add_node($textura)
+	#var colisaoAsset = assetOnMap.add_node($colisao)
+	#texturaAsset.texture = asset.texture
+	#texturaAsset.size = Vector2(asset.sizeX, asset.sizeY)
+	#colisaoAsset.size = Vector2(asset.sizeX, asset.sizeY)
+	
+	# Lógica de fazer um objeto para cada não funcionou, fazer usando tileMap e colcoar o asset no tileMapLayer na cena main
+	
+	# Problemas: 
+	# - não consegui colocar o tileset direto no valor dos assets, então será necessário transformar a textura em tile set OU *predefinir um tileset com a textura e só colocar os valores necessários para encontra-lo (as coordenadas, caso coloque o asset no tileMap padrão ou apenas o id do Tilemap caso separe em diferentes Tilemaps)*
+	# - Não sei como farei para mover os assets com eles como tileSets, talvez colocar para o mestre deletar tile por tile em vez de mover o tileSet completo
+	
 	# TODO: logica de colcoar o asset nop mapa
