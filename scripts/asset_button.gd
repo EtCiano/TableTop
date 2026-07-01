9640dd8f13c72ad46cda43b1a5db8b1e3627a616
EtCiano <miguelfmellobirck@gmail.com>
2026-07-01 10:26:38 -0300

    fiz um draw.io para me ajudar com a lógica feita pela IA para mostrar os assets/tokens no panel e tela

--- scripts/asset_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-var data: assetData
-
-func setup(asset: assetData) -> void:
-	data = asset
-	$VBoxContainer/Label.text = asset.name
-	$VBoxContainer/TextureRect.texture = asset.texture
